99c3c59aa3f2d40c0066ce4bfa30bdf0a857e45c
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2020-03-26 17:55:20 +0900

    test: add for linux.

---
+++ ledgertest.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+node debug-console.js ledger regtest "" 44h/0h/0h hood festival pony outdoor always jeans page help symptom adapt obtain image bird duty damage find sense wasp box mail vapor plug general kingdom

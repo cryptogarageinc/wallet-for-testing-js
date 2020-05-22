6651cb4d3183f4251f2f5916b5bfa60d093b8764
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2020-05-22 11:25:22 +0900

    style: code formatter

--- example_elements.js
+++ example_elements.js
@@ -10,7 +10,7 @@ const txdata = cfdjs.ElementsCreateRawTransaction({
     'txid': utxo.txid,
     'vout': utxo.vout,
     'sequence': 4294967295,
-  },{
+  }, {
     'txid': utxo2.txid,
     'vout': utxo2.vout,
     'sequence': 4294967295,
@@ -19,7 +19,7 @@ const txdata = cfdjs.ElementsCreateRawTransaction({
     'address': 'AzptfproXqXSz52kFUDAN4WJe3nVkxyapTkVkmN67Jmw4isdLWXrXF4jcF3354LcaQ5ZsBrvRmvWUz73',
     'asset': '6f1a4b6bd5571b5f08ab79c314dc6483f9b952af2f5ef206cd6f8e68eb1186f3',
     'amount': 10000000,
-  },{
+  }, {
     'address': 'el1qqtl9a3n6878ex25u0wv8u5qlzpfkycc0cftk65t52pkauk55jqka0fajk8d80lafn4t9kqxe77cu9ez2dyr6sq54lwy009uex',
     'asset': '6f1a4b6bd5571b5f08ab79c314dc6483f9b952af2f5ef206cd6f8e68eb1186f3',
     'amount': 10000000,
@@ -33,20 +33,20 @@ const txdata = cfdjs.ElementsCreateRawTransaction({
 const blindTx = cfdjs.BlindRawTransaction({
   tx: txdata.hex,
   txins: [{
-      txid: utxo.txid,
-      vout: utxo.vout,
-      asset: '6f1a4b6bd5571b5f08ab79c314dc6483f9b952af2f5ef206cd6f8e68eb1186f3',
-      amount: 10500000,
-      blindFactor: '0000000000000000000000000000000000000000000000000000000000000000',
-      assetBlindFactor: '0000000000000000000000000000000000000000000000000000000000000000',
-    },{
-      txid: utxo2.txid,
-      vout: utxo2.vout,
-      asset: '6f1a4b6bd5571b5f08ab79c314dc6483f9b952af2f5ef206cd6f8e68eb1186f3',
-      amount: 10000000,
-      blindFactor: 'f87734c279533d8beba96c5369e169e6caf5f307a34d72d4a0f9c9a7b8f8f269',
-      assetBlindFactor: '28093061ab2e407c6015f8cb33f337ffb118eaf3beb2d254de64203aa27ecbf7',
-    },
+    txid: utxo.txid,
+    vout: utxo.vout,
+    asset: '6f1a4b6bd5571b5f08ab79c314dc6483f9b952af2f5ef206cd6f8e68eb1186f3',
+    amount: 10500000,
+    blindFactor: '0000000000000000000000000000000000000000000000000000000000000000',
+    assetBlindFactor: '0000000000000000000000000000000000000000000000000000000000000000',
+  }, {
+    txid: utxo2.txid,
+    vout: utxo2.vout,
+    asset: '6f1a4b6bd5571b5f08ab79c314dc6483f9b952af2f5ef206cd6f8e68eb1186f3',
+    amount: 10000000,
+    blindFactor: 'f87734c279533d8beba96c5369e169e6caf5f307a34d72d4a0f9c9a7b8f8f269',
+    assetBlindFactor: '28093061ab2e407c6015f8cb33f337ffb118eaf3beb2d254de64203aa27ecbf7',
+  },
   ],
   txoutConfidentialAddresses: [
     'AzptfproXqXSz52kFUDAN4WJe3nVkxyapTkVkmN67Jmw4isdLWXrXF4jcF3354LcaQ5ZsBrvRmvWUz73',
@@ -61,9 +61,9 @@ const signTx = cfdjs.SignWithPrivkey({
   txin: {
     txid: utxo.txid,
     vout: utxo.vout,
-    privkey: "cU4KjNUT7GjHm7CkjRjG46SzLrXHXoH3ekXmqa2jTCFPMkQ64sw1",
-    hashType: "p2wpkh",
-    sighashType: "all",
+    privkey: 'cU4KjNUT7GjHm7CkjRjG46SzLrXHXoH3ekXmqa2jTCFPMkQ64sw1',
+    hashType: 'p2wpkh',
+    sighashType: 'all',
     confidentialValueCommitment: '096c711e318b1b540f683aa3ed570c571889417483979e24403e47d8be7c2ac664',
     amount: 10500000,
   },

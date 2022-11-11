61b640923ef3bbb21dafde310600aeb5fdc8c965
k-matsuzawa <tangrambs803@gmail.com>
2022-11-12 00:38:15 +0900

    feat: update library version, update ci image

--- __tests__/docker-bitcoin-entrypoint.sh
+++ __tests__/docker-bitcoin-entrypoint.sh
@@ -12,16 +12,11 @@ fi
 
 cd ${WORKDIR_PATH}
 rm -rf bitcoind_datadir
-rm -rf elementsd_datadir
 
 mkdir bitcoind_datadir
 chmod 777 bitcoind_datadir
 # cp /root/.bitcoin/bitcoin.conf bitcoind_datadir/
 cp ./__tests__/bitcoin.conf bitcoind_datadir/
-mkdir elementsd_datadir
-chmod 777 elementsd_datadir
-# cp /root/.elements/elements.conf elementsd_datadir/
-cp ./__tests__/elements.conf elementsd_datadir/
 
 # boot daemon
 bitcoind --regtest -datadir=${WORKDIR_PATH}/bitcoind_datadir
@@ -31,18 +26,6 @@ do
   bitcoin-cli --regtest -datadir=${WORKDIR_PATH}/bitcoind_datadir ping > /dev/null 2>&1
 done
 echo "start bitcoin node"
-
-elementsd -chain=liquidregtest -datadir=${WORKDIR_PATH}/elementsd_datadir -pak=02b6991705d4b343ba192c2d1b10e7b8785202f51679f26a1f2cdbe9c069f8dceb024fb0908ea9263bedb5327da23ff914ce1883f851337d71b3ca09b32701003d05
-elements-cli -chain=liquidregtest -datadir=${WORKDIR_PATH}/elementsd_datadir ping > /dev/null 2>&1
-while [ $? -ne 0 ]
-do
-  elements-cli -chain=liquidregtest -datadir=${WORKDIR_PATH}/elementsd_datadir ping > /dev/null 2>&1
-done
-echo "start elements node"
-
-# load or create wallet
-bitcoin-cli --regtest -datadir=${WORKDIR_PATH}/bitcoind_datadir createwallet wallet false false "" false false
-elements-cli -chain=liquidregtest -datadir=${WORKDIR_PATH}/elementsd_datadir createwallet wallet false false "" false false
 
 set -e
 
@@ -59,12 +42,12 @@ NODE_MAJOR_VER=$(node --version | sed -r 's/^v([0-9]+).([0-9]+).([0-9]+)(\..*)?$
 NODE_MINOR_VER=$(node --version | sed -r 's/^v([0-9]+).([0-9]+).([0-9]+)(\..*)?$/\2/')
 if [ $NODE_MAJOR_VER -gt 18 ]; then
   echo "node version $NODE_MAJOR_VER" ;
-  NODE_OPTIONS="--no-experimental-fetch" npm run ts-test ;
+  NODE_OPTIONS="--no-experimental-fetch" npm run js-test ;
 elif test "$NODE_MAJOR_VER" = "18" && test "$NODE_MINOR_VER" != "0"; then
   echo "node version $NODE_MAJOR_VER.$NODE_MINOR_VER" ;
-  NODE_OPTIONS="--no-experimental-fetch" npm run ts-test ;
+  NODE_OPTIONS="--no-experimental-fetch" npm run js-test ;
 else
-  npm run ts-test
+  npm run js-test
 fi
 
 rm -rf node_modules
